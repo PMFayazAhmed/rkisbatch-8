e6117c1b87e4f3a217eca3167647afe1e938e95b
Fayaz <fana.fayaz@gmail.com>
2023-10-06 07:48:42 +0530

    Added 2 sop statements

--- Sample.java
+++ Sample.java
@@ -3,5 +3,7 @@ class Sample
     public static void main(String args[])
     {
       System.out.println("This is a sample java program");
+      System.out.println("This is test statement");
+      System.out.println("Hello Java");
     }
   }
